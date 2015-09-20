c0d4abe08d9015c4fce98fdaff14502f3bc7b1ac
Philippe Muller <philippe.muller@gmail.com>
2015-09-20 21:01:07 +0200

    tmux: added new_tmux_django_session helper

---
+++ tmux/.tmux.functions.sh
@@ -0,0 +1,17 @@
+new_tmux_django_session()
+{
+    local SESSION=$1
+
+    if [[ -z "$SESSION" ]]
+    then
+        echo "Usage: create_tmux_django_session <name>"
+        return 1
+    fi
+
+    tmux new-session $SESSION_CREATE_ARGS -d -n code vim
+    tmux new-window -n shell
+    tmux new-window -n python "./manage.py shell"
+    tmux new-window -n server "./manage.py runserver"
+    tmux select-window -t code
+    tmux attach $SESSION_ATTACH_ARGS
+}
