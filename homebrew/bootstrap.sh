cd04628939c85fe18f258fa753325e787e8f944a
Philippe Muller <philippe.muller@gmail.com>
2015-09-20 16:02:21 +0200

    cleanup

--- homebrew/bootstrap.sh
+++ homebrew/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
+
 URL=https://raw.githubusercontent.com/Homebrew/install/master/install
+PACKAGES="autoconf bash cscope irssi lftp links macvim mplayer mtr
+          postgresql puppet python ruby stow tcptraceroute tmux unrar xz zsh"
+
 ruby -e "$(curl -fsSL $URL)"
-brew install autoconf bash cscope irssi lftp links macvim mplayer mtr \
-    postgresql puppet python ruby stow tcptraceroute tmux unrar xz zsh
+brew install $PACKAGES
