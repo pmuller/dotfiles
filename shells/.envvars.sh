073806ec1616dc8eb962b89d39aa38f04beac1a7
Philippe Muller <philippe.muller@gmail.com>
2016-01-23 16:18:35 +0100

    Add "node_modules/.bin" to $PATH
    When $CWD is a JS project root,
    scripts from packages installed by npm for this specific project
    overrides those installed on the system.

--- shells/.envvars.sh
+++ shells/.envvars.sh
@@ -3,7 +3,7 @@ PS1='\h:\w\$ '
 HISTFILE=~/.histfile
 HISTSIZE=20000
 SAVEHIST=10000
-export PATH=~/.conda/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+export PATH=node_modules/.bin:~/.conda/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 # Locales
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
