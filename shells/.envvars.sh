aaf314430c113ea82ced282e7943d30d73492cda
Philippe Muller <philippe.muller@gmail.com>
2016-04-29 10:59:56 +0200

    Use colors in man pages

--- shells/.envvars.sh
+++ shells/.envvars.sh
@@ -9,6 +9,15 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 # Default editor
 export EDITOR=vim
+# Less colors for man pages
+# src: https://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+export LESS_TERMCAP_mb=$'\e[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\e[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\e[0m'           # end mode
+export LESS_TERMCAP_se=$'\e[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\e[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\e[0m'           # end underline
+export LESS_TERMCAP_us=$'\e[04;38;5;146m' # begin underline
 
 if [ -z "$TMUX" ]
 then
