2b151ecae511b39c19866a910564955f5bc4b121
Philippe Muller <philippe.muller@gmail.com>
2016-06-23 19:11:35 +0200

    Add gpg-agent management script

---
+++ gpg-agent/.gpg-agent.sh
@@ -0,0 +1,46 @@
+_GPG_AGENT_LIFETIME=36000
+_GPG_AGENT_VARS=~/.gpg-agent.vars.$(hostname -f)
+
+
+_is_gpg_agent_running()
+{
+    if [ -n "$GPG_AGENT_INFO" ]
+    then
+        local PID=$(echo $GPG_AGENT_INFO | cut -d: -f2)
+        if [[ -n "$PID" && $PID > 0 ]]
+        then
+            if kill -0 $PID 2>/dev/null
+            then
+                return 0
+            else
+                return 1
+            fi
+        else
+            return 1
+        fi
+    else
+        return 1
+    fi
+}
+
+
+_run_gpg_agent()
+{
+    gpg-agent --daemon --use-standard-socket --default-cache-ttl $_GPG_AGENT_LIFETIME >"$_GPG_AGENT_VARS"
+    source "$_GPG_AGENT_VARS"
+}
+
+
+if [[ "$OSTYPE" =~ ^darwin ]]
+then
+    # Do nothing on OSX, rely on system's gpg-agent
+    :
+else
+    # Load last gpg-agent variables
+    if [[ -f "$_GPG_AGENT_VARS" ]]
+    then
+        source "$_GPG_AGENT_VARS"
+    fi
+    # Launch the gpg-agent if not already running
+    _is_gpg_agent_running || _run_gpg_agent
+fi
