ab22cfb2fdd2c09921fbd0ea0852a509d918312b
Philippe Muller <philippe.muller@gmail.com>
2016-07-10 16:52:46 +0200

    Make gpg-agent work on Mac

--- gpg-agent/.gpg-agent.sh
+++ gpg-agent/.gpg-agent.sh
@@ -26,21 +26,17 @@ _is_gpg_agent_running()
 
 _run_gpg_agent()
 {
-    gpg-agent --daemon --use-standard-socket --default-cache-ttl $_GPG_AGENT_LIFETIME >"$_GPG_AGENT_VARS"
+    gpg-agent --daemon --use-standard-socket --enable-ssh-support \
+        --default-cache-ttl $_GPG_AGENT_LIFETIME >"$_GPG_AGENT_VARS"
     source "$_GPG_AGENT_VARS"
 }
 
 
-if [[ "$OSTYPE" =~ ^darwin ]]
+# Load last gpg-agent variables
+if [[ -f "$_GPG_AGENT_VARS" ]]
 then
-    # Do nothing on OSX, rely on system's gpg-agent
-    :
-else
-    # Load last gpg-agent variables
-    if [[ -f "$_GPG_AGENT_VARS" ]]
-    then
-        source "$_GPG_AGENT_VARS"
-    fi
-    # Launch the gpg-agent if not already running
-    _is_gpg_agent_running || _run_gpg_agent
+    source "$_GPG_AGENT_VARS"
 fi
+
+# Launch the gpg-agent if not already running
+_is_gpg_agent_running || _run_gpg_agent
--- homebrew/bootstrap.sh
+++ homebrew/bootstrap.sh
@@ -4,5 +4,11 @@ URL=https://raw.githubusercontent.com/Homebrew/install/master/install
 PACKAGES="autoconf bash cscope irssi lftp links macvim mplayer mtr
           postgresql puppet python ruby stow tcptraceroute tmux unrar xz zsh"
 
+# Install Homebrew
 ruby -e "$(curl -fsSL $URL)"
+
+# Install gpgtools
+brew cask install gpgtools
+
+# Install other packages
 brew install $PACKAGES
