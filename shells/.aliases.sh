10186b4687574fb61b41e5ca1e818336c6bdd1a8
Philippe Muller <philippe.muller@gmail.com>
2016-08-18 19:52:09 +0800

    Remove Windows aliases
    Hopefully, I'll never need these again.

--- shells/.aliases.sh
+++ shells/.aliases.sh
@@ -13,21 +13,6 @@ then
     source /opt/rh/rh-postgresql95/enable
     alias pg_start='pg_ctl -D /var/run/postgresql start'
 
-elif [[ $OSTYPE =~ ^cygwin ]]
-then
-    # Web browser
-    alias www="/cygdrive/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe"
-    # Vagrant
-    alias vagrant="/cygdrive/c/HashiCorp/Vagrant/bin/vagrant.exe"
-    # Cygwin
-    alias cyginstall='cygsetup --quiet-mode --no-shortcuts --no-desktop --no-startmenu --root "$(cygpath -m /)" --local-package-dir "$(cygpath -w /packages)" --site $CYGWIN_MIRROR --packages'
-    # VirtualBox
-    alias VBoxManage='/cygdrive/c/Program\ Files/Oracle/VirtualBox/VBoxManage.exe'
-    # Use procps' ps (the cygwin standard "ps" does not show programs arguments)
-    alias ps=procps
-    # Use Windows' native ping, because Cygwin's ping requires admin privileges
-    alias ping=/cygdrive/c/Windows/System32/PING.EXE
-
 elif [[ $OSTYPE =~ ^darwin ]]
 then
     alias ls='ls -FGh'
