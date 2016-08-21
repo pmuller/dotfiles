c37b6639767b47c873de8b33ed56d839f77b714c
Philippe Muller <philippe.muller@gmail.com>
2016-08-21 21:10:16 +0800

    Auto load user-installed softwares

---
+++ shells/.local_software.sh
@@ -0,0 +1,21 @@
+_prepend_path_if_exists() {
+    local DIR=$1
+
+    if [[ -d $DIR ]]
+    then
+        export PATH=$DIR${PATH:+:${PATH}}
+    fi
+}
+
+
+_load_local_software() {
+    NAME=$1
+    PREFIX=${2-$HOME/local/$NAME}
+    _prepend_path_if_exists $PREFIX/bin
+    _prepend_path_if_exists $PREFIX/sbin
+}
+
+
+_load_local_software vim
+_load_local_software packer
+_load_local_software terraform
