e95b7c99d1b59a59d713f0c4c8db7239cb17efcf
Philippe Muller <philippe.muller@gmail.com>
2018-02-12 20:32:14 +0800

    Use pure-bash prompt

--- shells/.shell_prompt.sh
+++ shells/.shell_prompt.sh
@@ -1,341 +1,142 @@
-#
-# This shell prompt config file was created by promptline.vim
-#
-function prompt_is_root {
-    if [ $UID -eq 0 ]
-    then
-        echo -n "root"
-    fi
-}
-function __promptline_battery {
-  local percent_sign="%"
-  local battery_symbol=""
-  local threshold="25"
+# Prompt configuration
+_BASH_PROMPT_MAX_PATH_LENGTH=30
+_BASH_PROMPT_SHOW_FULL_PATH=0
+_BASH_PROMPT_HOST_PARTS=1
+_BASH_PROMPT_SHOW_LOCAL_USER_HOST=0
 
-  # escape percent "%" in zsh
-  [[ -n ${ZSH_VERSION-} ]] && percent_sign="${percent_sign//\%/%%}"
+# Ask virtualenv to NOT change PS1
+VIRTUAL_ENV_DISABLE_PROMPT=1
 
-  # osx
-  if hash ioreg 2>/dev/null; then
-    local ioreg_output
-    if ioreg_output=$(ioreg -rc AppleSmartBattery 2>/dev/null); then
-      local battery_capacity=${ioreg_output#*MaxCapacity\"\ \=}
-      battery_capacity=${battery_capacity%%\ \"*}
+# git-prompt.sh configuration
+GIT_PS1_SHOWCOLORHINTS=1
+GIT_PS1_SHOWUPSTREAM=auto
+GIT_PS1_SHOWUNTRACKEDFILES=1
+GIT_PS1_SHOWDIRTYSTATE=1
+GIT_PS1_SHOWSTASHSTATE=1
 
-      local current_capacity=${ioreg_output#*CurrentCapacity\"\ \=}
-      current_capacity=${current_capacity%%\ \"*}
+# Colors
+_COLOR_RESET="\033[0m"
+_COLOR_GREEN="\033[0;32m"
+_COLOR_YELLOW="\033[0;33m"
+_COLOR_BLUE="\033[0;34m"
+_COLOR_CYAN="\033[0;36m"
+_COLOR_BRIGHT_RED="\033[1;31m"
+_COLOR_DARK_GREY="\033[38;5;240m"
+_COLOR_GREY="\033[38;5;248m"
+# Unicode chars
+_UNICODE_ELLIPSIS="\xe2\x80\xa6"
 
-      local battery_level=$(($current_capacity * 100 / $battery_capacity))
-      [[ $battery_level -gt $threshold ]] && return 1
+# Default git-prompt.sh location (at least, on CentOS 7)
+_GIT_PROMPT=/usr/share/git-core/contrib/completion/git-prompt.sh
 
-      printf "%s" "${battery_symbol}${battery_level}${percent_sign}"
-      return
-    fi
-  fi
-
-  # linux
-  for possible_battery_dir in /sys/class/power_supply/BAT*; do
-    if [[ -d $possible_battery_dir && -f "$possible_battery_dir/charge_full" && -f "$possible_battery_dir/charge_now" ]]; then
-      current_capacity=$( <"$possible_battery_dir/charge_now" )
-      battery_capacity=$( <"$possible_battery_dir/charge_full" )
-      local battery_level=$(($current_capacity * 100 / $battery_capacity))
-      [[ $battery_level -gt $threshold ]] && return 1
-
-      printf "%s" "${battery_symbol}${battery_level}${percent_sign}"
-      return
-    fi
-  done
-
-return 1
-}
-function __promptline_ps1 {
-  local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
-
-  # section "a" header
-  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "a" slices
-  __promptline_wrapper "$(prompt_is_root)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-  __promptline_wrapper "$(__promptline_battery)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-  __promptline_wrapper "$(__promptline_jobs)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "b" header
-  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "b" slices
-  __promptline_wrapper "$_vi_mode" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-  __promptline_wrapper "${VIRTUAL_ENV##*/}" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "c" header
-  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "x" header
-  slice_prefix="${x_bg}${sep}${x_fg}${x_bg}${space}" slice_suffix="$space${x_sep_fg}" slice_joiner="${x_fg}${x_bg}${alt_sep}${space}" slice_empty_prefix="${x_fg}${x_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "x" slices
-  __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "y" header
-  slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "y" slices
-  __promptline_wrapper "$(__promptline_git_status)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "z" header
-  slice_prefix="${z_bg}${sep}${z_fg}${z_bg}${space}" slice_suffix="$space${z_sep_fg}" slice_joiner="${z_fg}${z_bg}${alt_sep}${space}" slice_empty_prefix="${z_fg}${z_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "z" slices
-  __promptline_wrapper "$(date +%H:%M:%S)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "warn" header
-  slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}" slice_empty_prefix="${warn_fg}${warn_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "warn" slices
-  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # close sections
-  printf "%s" "${reset_bg}${sep}$reset$space"
-}
-function __promptline_vcs_branch {
-  local branch
-  local branch_symbol=" "
-
-  # git
-  if hash git 2>/dev/null; then
-    if branch=$( { git symbolic-ref --quiet HEAD || git rev-parse --short HEAD; } 2>/dev/null ); then
-      branch=${branch##*/}
-      printf "%s" "${branch_symbol}${branch:-unknown}"
-      return
-    fi
-  fi
-  return 1
-}
-
-function __promptline_jobs {
-  local job_count=0
-
-  local IFS=$'\n'
-  for job in $(jobs); do
-    # count only lines starting with [
-    if [[ $job == \[* ]]; then
-      job_count=$(($job_count+1))
-    fi
-  done
-
-  [[ $job_count -gt 0 ]] || return 1;
-  printf "%s" "$job_count"
-}
-function __promptline_left_prompt {
-  local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
-
-  # section "a" header
-  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "a" slices
-  __promptline_wrapper "$(prompt_is_root)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-  __promptline_wrapper "$(__promptline_battery)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-  __promptline_wrapper "$(__promptline_jobs)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "b" header
-  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "b" slices
-  __promptline_wrapper "$_vi_mode" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-  __promptline_wrapper "${VIRTUAL_ENV##*/}" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "c" header
-  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # close sections
-  printf "%s" "${reset_bg}${sep}$reset$space"
-}
-function __promptline_wrapper {
-  # wrap the text in $1 with $2 and $3, only if $1 is not empty
-  # $2 and $3 typically contain non-content-text, like color escape codes and separators
-
-  [[ -n "$1" ]] || return 1
-  printf "%s" "${2}${1}${3}"
-}
-function __promptline_git_status {
-  [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]] || return 1
-
-  local added_symbol="●"
-  local unmerged_symbol="✗"
-  local modified_symbol="+"
-  local clean_symbol="✔"
-  local has_untracked_files_symbol="…"
-
-  local ahead_symbol="↑"
-  local behind_symbol="↓"
-
-  local unmerged_count=0 modified_count=0 has_untracked_files=0 added_count=0 is_clean=""
-
-  set -- $(git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null)
-  local behind_count=$1
-  local ahead_count=$2
-
-  # Added (A), Copied (C), Deleted (D), Modified (M), Renamed (R), changed (T), Unmerged (U), Unknown (X), Broken (B)
-  while read line; do
-    case "$line" in
-      M*) modified_count=$(( $modified_count + 1 )) ;;
-      U*) unmerged_count=$(( $unmerged_count + 1 )) ;;
-    esac
-  done < <(git diff --name-status)
-
-  while read line; do
-    case "$line" in
-      *) added_count=$(( $added_count + 1 )) ;;
-    esac
-  done < <(git diff --name-status --cached)
-
-  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-    has_untracked_files=1
-  fi
-
-  if [ $(( unmerged_count + modified_count + has_untracked_files + added_count )) -eq 0 ]; then
-    is_clean=1
-  fi
-
-  local leading_whitespace=""
-  [[ $ahead_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$ahead_symbol$ahead_count"; leading_whitespace=" "; }
-  [[ $behind_count -gt 0 ]]        && { printf "%s" "$leading_whitespace$behind_symbol$behind_count"; leading_whitespace=" "; }
-  [[ $modified_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$modified_symbol$modified_count"; leading_whitespace=" "; }
-  [[ $unmerged_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$unmerged_symbol$unmerged_count"; leading_whitespace=" "; }
-  [[ $added_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$added_symbol$added_count"; leading_whitespace=" "; }
-  [[ $has_untracked_files -gt 0 ]] && { printf "%s" "$leading_whitespace$has_untracked_files_symbol"; leading_whitespace=" "; }
-  [[ $is_clean -gt 0 ]]            && { printf "%s" "$leading_whitespace$clean_symbol"; leading_whitespace=" "; }
-}
-
-function __promptline_last_exit_code {
-
-  [[ $last_exit_code -gt 0 ]] || return 1;
-
-  printf "%s" "$last_exit_code"
-}
-function __promptline_right_prompt {
-  local slice_prefix slice_empty_prefix slice_joiner slice_suffix
-
-  # section "warn" header
-  slice_prefix="${warn_sep_fg}${rsep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_rsep}${space}" slice_empty_prefix=""
-  # section "warn" slices
-  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
-
-  # section "x" header
-  slice_prefix="${x_sep_fg}${rsep}${x_fg}${x_bg}${space}" slice_suffix="$space${x_sep_fg}" slice_joiner="${x_fg}${x_bg}${alt_rsep}${space}" slice_empty_prefix=""
-  # section "x" slices
-  __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
-
-  # section "y" header
-  slice_prefix="${y_sep_fg}${rsep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_rsep}${space}" slice_empty_prefix=""
-  # section "y" slices
-  __promptline_wrapper "$(__promptline_git_status)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
-
-  # section "z" header
-  slice_prefix="${z_sep_fg}${rsep}${z_fg}${z_bg}${space}" slice_suffix="$space${z_sep_fg}" slice_joiner="${z_fg}${z_bg}${alt_rsep}${space}" slice_empty_prefix=""
-  # section "z" slices
-  __promptline_wrapper "$(date +%H:%M:%S)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
-
-  # close sections
-  printf "%s" "$reset"
-}
-function __promptline_cwd {
-  local dir_limit="3"
-  local truncation="⋯"
-  local first_char
-  local part_count=0
-  local formatted_cwd=""
-  local dir_sep="  "
-  local tilde="~"
-
-  local cwd="${PWD/#$HOME/$tilde}"
-
-  # get first char of the path, i.e. tilde or slash
-  [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
-
-  # remove leading tilde
-  cwd="${cwd#\~}"
-
-  while [[ "$cwd" == */* && "$cwd" != "/" ]]; do
-    # pop off last part of cwd
-    local part="${cwd##*/}"
-    cwd="${cwd%/*}"
-
-    formatted_cwd="$dir_sep$part$formatted_cwd"
-    part_count=$((part_count+1))
-
-    [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
-  done
-
-  printf "%s" "$first_char$formatted_cwd"
-}
-function __promptline {
-  local last_exit_code="${PROMPTLINE_LAST_EXIT_CODE:-$?}"
-
-  local esc=$'[' end_esc=m
-  if [[ -n ${ZSH_VERSION-} ]]; then
-    local noprint='%{' end_noprint='%}'
-  elif [[ -n ${FISH_VERSION-} ]]; then
-    local noprint='' end_noprint=''
-  else
-    local noprint='\[' end_noprint='\]'
-  fi
-  local wrap="$noprint$esc" end_wrap="$end_esc$end_noprint"
-  local space=" "
-  local sep=""
-  local rsep=""
-  local alt_sep=""
-  local alt_rsep=""
-  local reset="${wrap}0${end_wrap}"
-  local reset_bg="${wrap}49${end_wrap}"
-  local a_fg="${wrap}38;5;220${end_wrap}"
-  local a_bg="${wrap}48;5;166${end_wrap}"
-  local a_sep_fg="${wrap}38;5;166${end_wrap}"
-  local b_fg="${wrap}38;5;231${end_wrap}"
-  local b_bg="${wrap}48;5;31${end_wrap}"
-  local b_sep_fg="${wrap}38;5;31${end_wrap}"
-  local c_fg="${wrap}38;5;250${end_wrap}"
-  local c_bg="${wrap}48;5;240${end_wrap}"
-  local c_sep_fg="${wrap}38;5;240${end_wrap}"
-  local warn_fg="${wrap}38;5;231${end_wrap}"
-  local warn_bg="${wrap}48;5;52${end_wrap}"
-  local warn_sep_fg="${wrap}38;5;52${end_wrap}"
-  local x_fg="${wrap}38;5;250${end_wrap}"
-  local x_bg="${wrap}48;5;236${end_wrap}"
-  local x_sep_fg="${wrap}38;5;236${end_wrap}"
-  local y_fg="${wrap}38;5;250${end_wrap}"
-  local y_bg="${wrap}48;5;236${end_wrap}"
-  local y_sep_fg="${wrap}38;5;236${end_wrap}"
-  local z_fg="${wrap}38;5;250${end_wrap}"
-  local z_bg="${wrap}48;5;236${end_wrap}"
-  local z_sep_fg="${wrap}38;5;236${end_wrap}"
-  if [[ -n ${ZSH_VERSION-} ]]; then
-    PROMPT="$(__promptline_left_prompt)"
-    RPROMPT="$(__promptline_right_prompt)"
-  elif [[ -n ${FISH_VERSION-} ]]; then
-    if [[ -n "$1" ]]; then
-      [[ "$1" = "left" ]] && __promptline_left_prompt || __promptline_right_prompt
-    else
-      __promptline_ps1
-    fi
-  else
-    PS1="$(__promptline_ps1)"
-  fi
-}
-
-if [[ -n ${ZSH_VERSION-} ]]; then
-  if [[ ! ${precmd_functions[(r)__promptline]} == __promptline ]]; then
-    precmd_functions+=(__promptline)
-  fi
-elif [[ -n ${FISH_VERSION-} ]]; then
-  __promptline "$1"
-else
-  if [[ ! "$PROMPT_COMMAND" == *__promptline* ]]; then
-    PROMPT_COMMAND='__promptline;'$'\n'"$PROMPT_COMMAND"
-  fi
+# Load git-prompt.sh if available
+if [ -f "$_GIT_PROMPT" ]
+then
+    . "$_GIT_PROMPT"
 fi
+
+
+# Helpers
+_ps1_git() {
+    if type __git_ps1 >/dev/null 2>&1
+    then
+        __git_ps1 "$_COLOR_GREEN%s$_COLOR_RESET "
+    fi
+}
+
+_ps1_user_host() {
+    if [ -n "$SSH_CONNECTION" ] || \
+        [ "$_BASH_PROMPT_SHOW_LOCAL_USER_HOST" != "0" ]
+    then
+        local HOST
+
+        if [ "$_BASH_PROMPT_HOST_PARTS" -gt 0 ]
+        then
+            # When positive, show first N parts
+            HOST=$(echo "$HOSTNAME" | cut -d. -f-$_BASH_PROMPT_HOST_PARTS)
+        elif [ "$_BASH_PROMPT_HOST_PARTS" -lt 0 ]
+        then
+            # When negative, hide last N parts
+            local CUT_START_FIELD=$(( "$_BASH_PROMPT_HOST_PARTS" + 1 ))
+            HOST=$(echo "$HOSTNAME" | rev | cut -d. -f$CUT_START_FIELD- | rev)
+        else  # _BASH_PROMPT_HOST_PARTS=0
+            # When 0, show only the first part
+            HOST=${HOSTNAME/.*}
+        fi
+
+        # Hide "root@" because the hash at the end of the prompt already
+        # gives the information
+        if [ "$(id -u)" -ne 0 ]
+        then
+            echo -ne "$_COLOR_GREY$USER$_COLOR_DARK_GREY@$_COLOR_RESET"
+        fi
+
+        if [[ "$HOSTNAME" =~ production ]]
+        then
+            echo -ne "$_COLOR_BRIGHT_RED$HOST$_COLOR_RESET "
+        else
+            echo -ne "$_COLOR_GREY$HOST$_COLOR_RESET "
+        fi
+    fi
+}
+
+_ps1_last_exit_code() {
+    local EXIT_CODE=$?
+
+    if [ "$EXIT_CODE" -ne 0 ]
+    then
+        echo -ne "$_COLOR_BRIGHT_RED$EXIT_CODE$_COLOR_RESET "
+    fi
+}
+
+_ps1_job_count() {
+    local JOB_COUNT="$(jobs | wc -l)"
+
+    if [ "$JOB_COUNT" -gt 0 ]
+    then
+        echo -ne "$_COLOR_YELLOW$JOB_COUNT$_COLOR_RESET "
+    fi
+}
+
+_ps1_virtualenv() {
+    if [ -n "$VIRTUAL_ENV" ]
+    then
+        local SHORT_PATH=$(echo "$VIRTUAL_ENV" | rev | cut -d/ -f1-2 | rev)
+
+        if [[ $SHORT_PATH =~ /\.env$ ]]
+        then
+            SHORT_PATH=${SHORT_PATH/\/*}
+        fi
+
+        echo -ne "$_COLOR_CYAN$SHORT_PATH$_COLOR_RESET "
+    fi
+}
+
+_ps1_cwd() {
+    local DIR=${PWD//$HOME/\~}  # Replace $HOME with ~
+    local LENGTH=${#DIR}
+
+    # Shorten, if necessary
+    if [ "$LENGTH" -gt "$_BASH_PROMPT_MAX_PATH_LENGTH" ] && \
+        [ "$_BASH_PROMPT_SHOW_FULL_PATH" -eq 0 ]
+    then
+        local LAST_3=$(echo "$PWD" | rev | cut -d/ -f1-3 | rev)
+        echo -e "$_COLOR_BLUE$_UNICODE_ELLIPSIS/$LAST_3$_COLOR_RESET "
+    else
+        echo -e "$_COLOR_BLUE$DIR$_COLOR_RESET "
+    fi
+}
+
+_ps1_id_symbol() {
+    if [ "$(id -u)" -eq 0 ]
+    then
+        # Show the hash in bright red, because we should never forget that
+        # doing stuff as root is rarely necessary...
+        echo -ne "$_COLOR_BRIGHT_RED#$_COLOR_RESET "
+    else
+        echo -ne "$_COLOR_GREY\$$_COLOR_RESET "
+    fi
+}
+
+
+PS1="\$(_ps1_last_exit_code)\$(_ps1_job_count)$(_ps1_user_host)\$(_ps1_virtualenv)\$(_ps1_cwd)\$(_ps1_git)$(_ps1_id_symbol)"
