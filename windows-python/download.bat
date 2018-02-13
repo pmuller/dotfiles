81b0344bb5764c0b086d28ac275c9504bd8262b3
Philippe Muller <philippe.muller@gmail.com>
2018-02-14 01:07:59 +0800

    Remove Windows and Mac OS X stuff

--- windows-python/download.bat
+++
@@ -1,9 +0,0 @@
-set URL=%1
-set TARGET=%2
-
-if exist %TARGET% (
-    @echo Re-using previously downloaded %TARGET%
-) else (
-    @echo Downloading %TARGET% from %URL% ...
-    @bitsadmin /transfer "setuptools" %URL% %TARGET%
-)
